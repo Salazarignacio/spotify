4d5d1d6771690e1b01ac02b99066060f21745db0
Ignacio <ignaciosalazar986@gmail.com>
2024-09-14 12:40:15 -0300

    changes

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
-  styleUrls: ['./media-player.component.css']
+  styleUrls: ['./media-player.component.css'],
 })
-export class MediaPlayerComponent {
+export class MediaPlayerComponent implements OnInit {
+  mockCover: any = {
+    cover: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/%C3%81ngel_Di_Mar%C3%ADa_2017.jpg',
+    album: 'zz',
+    name: 'nacho',
+  };
+  constructor() {}
 
+  ngOnInit(): void {}
 }
